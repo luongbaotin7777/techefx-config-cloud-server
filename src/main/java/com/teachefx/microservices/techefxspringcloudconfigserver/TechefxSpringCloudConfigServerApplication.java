9bf3ddbb630ef91912c0d37a641685bb496c331e
Tin Luong <lbtin@tma.com.vn>
2021-03-19 16:43:29 +0700

    first commit

---
+++ src/main/java/com/teachefx/microservices/techefxspringcloudconfigserver/TechefxSpringCloudConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.teachefx.microservices.techefxspringcloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class TechefxSpringCloudConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechefxSpringCloudConfigServerApplication.class, args);
+    }
+
+}
